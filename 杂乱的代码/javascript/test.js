6ae35c4faae952cdab50613370f502ba97e503b4
ℳ๓₯㎕白月初 <42824008+Silence-dream@users.noreply.github.com>
2020-01-07 14:41:33 +0800

    笔记更新

---
+++ 杂乱的代码/javascript/test.js
@@ -0,0 +1,41 @@
+// console.log(b);
+// console.log(b());
+
+// var b
+//     = 1;
+// function b() {
+//     return 2;
+// }
+
+
+// function test() {
+//     alert(4);
+// }
+// var test;
+
+
+// console.log(test);
+
+
+console.log(a);//  [Function:a] 为什么不是undefined呢？
+a(); 2
+function a() {
+    console.log(10)//10
+}
+var a = 3;
+
+
+// console.log(a)
+// function a() {
+//     console.log('helloworld')
+// }
+
+// function a() { console.log('hello') };
+// var a;
+// console.log(a)//ƒ a(){console.log('hello')}
+
+// function a() { console.log('hello') };
+// var a = undefined;
+// console.log(a)//undefined
+
+
---
+++ 杂乱的代码/javascript/test2.js
@@ -0,0 +1,7 @@
+var a = 1;
+
+function a() {
+    return 2;
+}
+console.log(a);
+console.log(a());
