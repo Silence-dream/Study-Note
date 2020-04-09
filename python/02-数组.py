63ce3920899b1ee17647ab7d2486f956eaaf3043
ℳ๓₯㎕白月初 <42824008+Silence-dream@users.noreply.github.com>
2020-04-09 22:59:23 +0800

    提交一些 py交易

---
+++ python/02-数组.py
@@ -0,0 +1,11 @@
+import numpy as np
+# numpy区间
+arr1 = np.arange(1, 10, 1, int)
+print(arr1)
+for value in arr1:
+    print(value)
+    # Python区间
+arr2 = range(1, 5, 1)
+print(arr2)
+for value in arr2:
+    print(value)
