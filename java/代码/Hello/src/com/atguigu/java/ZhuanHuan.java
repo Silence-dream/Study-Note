c6c545798c35f1f664941466c0c379a004d6b1a4
ℳ๓₯㎕白月初 <42824008+Silence-dream@users.noreply.github.com>
2020-05-06 10:53:33 +0800

    提交代码

---
+++ java/代码/Hello/src/com/atguigu/java/ZhuanHuan.java
@@ -0,0 +1,9 @@
+package com.atguigu.java;
+
+public class ZhuanHuan {
+    public static void main(String[] args) {
+        // 强制大转 小
+        int a = (short) 100;
+        System.out.println(a);
+    }
+}
--- 杂乱的代码/JavaScript高级/02-类中添加方法(1).js
+++ 杂乱的代码/JavaScript高级/02-类中添加方法(1).js
@@ -1,18 +1,19 @@
 // 1. 创建类 class  创建一个 明星类
 class Star {
     // 类的共有属性放到 constructor 里面
-    constructor(uname, age) {
-        this.uname = uname;
-        this.age = age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age
     }
     sing(song) {
-        // console.log('我唱歌');
-        console.log(this.uname + song);
+        console.log(this.name + "唱" + song);
     }
 }
+
+
 // 2. 利用类创建对象 new
-var ldh = new Star('刘德华', 18);
-var zxy = new Star('张学友', 20);
+var ldh = new Star("刘德华", 18);
+var zxy = new Star("张学友", 18);
 console.log(ldh);
 console.log(zxy);
 // (1) 我们类里面所有的函数不需要写function 
