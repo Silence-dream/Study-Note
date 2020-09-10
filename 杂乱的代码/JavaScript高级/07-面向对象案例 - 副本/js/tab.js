018a6add7e07e26ce16c57177a9c6ded490aa394
ℳ๓₯㎕白月初 <42824008+Silence-dream@users.noreply.github.com>
2020-09-10 09:16:27 +0800

    update:面向对象tab案例

--- 杂乱的代码/JavaScript高级/07-面向对象案例 - 副本/js/tab.js
+++ 杂乱的代码/JavaScript高级/07-面向对象案例 - 副本/js/tab.js
@@ -4,7 +4,6 @@ class Tab {
         that = this;
         // 得到大盒子
         this.tabsbox = document.querySelector(id);
-
         // console.log(this.section);
         // 添加按钮
         this.add = this.tabsbox.querySelector(".tabadd");
@@ -24,22 +23,31 @@ class Tab {
             this.lis[i].index = i;
             this.lis[i].section = i;
             this.lis[i].onclick = this.toggle;
+            // 删除按钮绑定点击事件
+            this.remove[i].onclick = this.deleteTab;
+            this.spans[i].ondblclick = this.editTab;
+            this.section[i].ondblclick = this.editTab;
         }
         // 添加按钮
         this.add.onclick = this.addTab;
+
     }
     // 更新节点获取
     updataNode() {
         // 得到tab标签
         this.lis = document.querySelectorAll(".fisrstnav li");
+        // 得到span
+        this.spans = document.querySelectorAll(".fisrstnav li span:first-child");
         // console.log(this.lis);
         // 得到tab内容标签
         this.section = document.querySelectorAll(".tabscon section");
+        // 得到删除按钮
+        this.remove = this.ul.querySelectorAll(".icon-guanbi");
     }
     //切换功能
     toggle() {
         that.clearName();
-        console.log(this.index);
+        // console.log(this.index);
         this.className = "liactive";
         that.section[this.index].className = "conactive";
         // console.log(that.section);
@@ -62,9 +70,50 @@ class Tab {
         that.init();
     }
     //删除功能
-    delete() {}
+    deleteTab(e) {
+        e.stopPropagation();
+        // 得到点击的索引
+        let cIndex = this.parentNode.index;
+        // 标签删除
+        this.parentNode.remove()
+        // 标签内容删除
+        that.section[cIndex].remove()
+        // 初始化
+        that.init();
+        // 如果点击的有.liactive这个类就不执行下面的代码
+        // console.log(this.parentNode)
+        if (document.querySelector(".liactive")) return false;
+        // 点击删除让前一个标签选中
+        that.lis[cIndex - 1] && that.lis[cIndex - 1].click();
+    }
     //修改功能
-    edit() {}
+    editTab(e) {
+        window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
+        // console.log(this)
+        console.dir(this)
+        if (this.tagName == "SPAN" || this.tagName == "SECTION") {
+            // 得到span里面的文字
+            let spanText = this.innerHTML;
+            this.innerHTML = `<input type="text" value='${spanText}'/>`;
+            // 选中input
+            let input = this.children[0];
+            input.select();
+
+            // 失去焦点把input的内容给span
+            input.onblur = function () {
+                this.parentNode.innerHTML = input.value;
+            }
+
+            input.onkeyup = function (e) {
+                if (e.keyCode == 13) {
+                    input.blur();
+                }
+            }
+        }
+
+
+
+    }
 }
 
 new Tab("#tab");
