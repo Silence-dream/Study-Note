0dbca4cc112436829e3f3573952165fbd3123421
ℳ๓₯㎕白月初 <42824008+Silence-dream@users.noreply.github.com>
2020-10-20 11:17:23 +0800

    代码更新

---
+++ webpack/webpack-html/src/index.js
@@ -0,0 +1,14 @@
+// require("./css/index.css");
+//
+// let div = document.createElement("div");
+// div.innerHTML = "你好webpack css-loader and style-loader";
+// document.body.appendChild(div);
+
+let 数组 = [1, 2, 3, 4];
+console.log(数组);
+
+function 函数(a, b) {
+  return a + b;
+}
+
+console.log(函数(1, 2));
