f74793a9edc25a944407c4c734b7a4be927e1085
ℳ๓₯㎕白月初 <42824008+Silence-dream@users.noreply.github.com>
2020-11-19 17:17:18 +0800

    代码移动

--- Node/express/app.js
+++ Node/express/app.js
@@ -1,36 +1,44 @@
-/*
- * @Author: ℳ๓₯㎕白月初
- * @Date: 2020-11-12 19:47:06
- * @LastEditors: OBKoro1
- * @LastEditTime: 2020-11-16 19:47:27
- * @FilePath: \express\app.js
- */
+// /*
+//  * @Author: ℳ๓₯㎕白月初
+//  * @Date: 2020-11-12 19:47:06
+//  * @LastEditors: OBKoro1
+//  * @LastEditTime: 2020-11-16 19:47:27
+//  * @FilePath: \express\app.js
+//  */
+
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+
+// var mysql = require("mysql");
+// console.log(process.env);
+// // export process.env.MySQL_HOUST="127.0.0.1"
+// // export process.env.MySQL_USERNAME="root"
+// // export process.env.MySQL_PASSWORLD="root"
+// const pool = mysql.createPool({
+//   //支持多条sql语句
+//   multipleStatements: true,
+//   //数据库连接池 有多少连接
+//   connecttionLimit: 20,
+//   host: process.env.MySQL_HOUST,
+//   user: process.env.MySQL_USERNAME,
+//   password: process.env.MySQL_PASSWORLD,
+//   database: "test1",
+// });
+
+// pool.query("select * from user", function (error, results, fields) {
+//   // if (error) throw error;
+//   // console.log('The solution is: ', results[0].solution);
+//   console.log(results);
+// });
+
+// app.get("/", (req, res) => res.send("Hello World!"));
+// app.listen(port, () => console.log(`Example app listening on port port!`));
 
 const express = require("express");
 const app = express();
 const port = 3000;
 
-var mysql = require("mysql");
-console.log(process.env);
-// export process.env.MySQL_HOUST="127.0.0.1"
-// export process.env.MySQL_USERNAME="root"
-// export process.env.MySQL_PASSWORLD="root"
-const pool = mysql.createPool({
-  //支持多条sql语句
-  multipleStatements: true,
-  //数据库连接池 有多少连接
-  connecttionLimit: 20,
-  host: process.env.MySQL_HOUST,
-  user: process.env.MySQL_USERNAME,
-  password: process.env.MySQL_PASSWORLD,
-  database: "test1",
-});
+app.use(express.static("dist"));
 
-pool.query("select * from user", function (error, results, fields) {
-  // if (error) throw error;
-  // console.log('The solution is: ', results[0].solution);
-  console.log(results);
-});
-
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port port!`));
+app.listen(port, () => console.log(`http://localhost:${port}`));
