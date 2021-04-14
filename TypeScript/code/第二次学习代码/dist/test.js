bdb677096cefcf3b6ccedd30ed7e63ce7026c8fe
Silence-dream <1428482231@qq.com>
2021-04-14 09:08:28 +0800

    TS提交更新

--- TypeScript/code/第二次学习代码/dist/test.js
+++ TypeScript/code/第二次学习代码/dist/test.js
@@ -1,19 +1,15 @@
+"use strict";
 // console.log(1);
-
 // let flag: any = 1223;
-
 // // as 的作用就是告诉编译器 我比你更懂 flag 现在是什么类型
 // console.log((flag as string).length);
-
 // 确定赋值断言
 // let aa: number;
 // jisuan();
 // console.log(2 * aa);
-
 // function jisuan() {
 //   aa = 10;
 // }
-
-let a: any = 5;
-let b: number = a;
-let c: unknown = 6;
+var a = 5;
+var b = a;
+var c = 6;
