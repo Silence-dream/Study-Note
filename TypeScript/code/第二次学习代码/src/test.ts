9d455f0e365f36a88f878089475b97755dbfcba8
Silence-dream <1428482231@qq.com>
2021-04-16 21:40:06 +0800

    git  和 ts 代码修改

--- TypeScript/code/第二次学习代码/src/test.ts
+++ TypeScript/code/第二次学习代码/src/test.ts
@@ -1,9 +1,21 @@
-function reverse(x: number): number;
-function reverse(x: string): string;
-function reverse(x: number | string) {
-  if (typeof x === "number") {
-    return Number(x.toString().split("").reverse().join(""));
-  } else if (typeof x === "string") {
-    return x.split("").reverse().join("");
-  }
-}
+let an: unknown;
+
+let f: any;
+
+f = an;
+
+an = "asd";
+
+let b: undefined;
+b = undefined;
+type A = { a: string };
+let obj: A = { a: "1" };
+// in 会检测 key 是否在对象上面如果有就返回 true
+// in 会检测原型链 toString 就在原型链上面被检测出来了
+console.log("a" in obj);
+console.log("toString" in obj);
+// in 与 hasOwnProperty 区别
+console.log(obj.hasOwnProperty("a"));
+console.log(obj.hasOwnProperty("toString"));
+
+export {};
--- TypeScript/code/第二次学习代码/src/类型守卫.ts
+++ TypeScript/code/第二次学习代码/src/类型守卫.ts
@@ -1,12 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var an;
-var f;
-f = an;
-an = "asd";
-var b;
-b = undefined;
-var obj = { a: "1" };
+let obj = { a: "1" };
 // in 会检测 key 是否在对象上面如果有就返回 true
 // in 会检测原型链 toString 就在原型链上面被检测出来了
 console.log("a" in obj);
@@ -14,3 +6,5 @@ console.log("toString" in obj);
 // in 与 hasOwnProperty 区别
 console.log(obj.hasOwnProperty("a"));
 console.log(obj.hasOwnProperty("toString"));
+
+export {};
