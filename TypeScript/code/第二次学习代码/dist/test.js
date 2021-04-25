781f5be991ff4457c8eb237da199a8fe2859b85d
Silence-dream <1428482231@qq.com>
2021-04-25 21:03:51 +0800

    Object.defineProperty

--- TypeScript/code/第二次学习代码/dist/test.js
+++ TypeScript/code/第二次学习代码/dist/test.js
@@ -1,25 +1,3 @@
 "use strict";
-let p = { name: "张三", age: 10 };
-let b = p;
-//类Person
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-}
-//类充当接口使用,接口中只包含其中的实例属性和实例方法（constructor和static不在其中）
-const person = {
-  name: "1",
-  age: 18,
-};
-let fn1 = (x, y) => {
-  console.log("fn1");
-};
-let fn2 = (x) => {
-  console.log(x);
-};
-fn1 = fn2;
-// fn2 = fn1
-
-function fn5(x, y) {}
+// Object.defineProperty;
+Object.defineProperty(exports, "__esModule", { value: true });
--- TypeScript/code/第二次学习代码/src/test.ts
+++ TypeScript/code/第二次学习代码/src/test.ts
@@ -1,15 +1,34 @@
-function add(x1: number, x2: number): number;
-function add(x1: string, x2: string): string;
+// Object.defineProperty;
 
-function add(
-  x1: number | string,
-  x2: number | string
-): number | string | undefined {
-  if (typeof x1 == "number" && typeof x2 == "number") {
-    return x1 + x2;
-  } else if (typeof x1 == "string" && typeof x2 == "string") {
-    return x1 + x2;
-  }
-}
-let result = add(1, 2);
-console.log(result);
+// 1.	数据描述符，它拥有四个属性配置
+// 2.	configurable：数据是否可删除，可配置
+// 3.	enumerable：属性是否可枚举
+// 4.	value：属性值,默认为undefined
+// 5.	writable：属性是否可读写
+// 6.	get:一个给属性提供 getter 的方法，如果没有 getter 则为 undefined。
+// 7.	set:一个给属性提供 setter 的方法，如果没有 setter 则为 undefined。
+
+// let obj = { name: "123", age: 18 };
+// Object.defineProperty(obj, "name", {
+//   configurable: false, // name 不可删除
+//   enumerable: false, // name 不可便利出来
+//   writable: false, // name 的值无法改写
+// });
+// for (const key in obj) {
+//   console.log(key, obj[key]); // 18
+// }
+// delete obj.name;
+// obj.name = "李四"; // 无法改写
+// console.log(obj); // { name: "123", age: 18 }
+// Object.defineProperty(obj, "age", {
+//   value: "age的值被修改了",
+// });
+// console.log(obj); // {age: "age的值被修改了", name: "123"}
+
+// let obj2 = { name: "李四", age: 90 };
+// Object.defineProperty(obj,"name",{
+//     get:(value){
+//         console.log(value);
+//     }
+// })
+export {};
