1e57a28fd2bf1458fddddd6dd3dc054a6956a7d8
Silence-dream <1428482231@qq.com>
2021-05-12 08:46:32 +0800

    react context

--- React/react-demo/src/App.js
+++ React/react-demo/src/App.js
@@ -3,6 +3,7 @@ import Fncom from "./components/Fncom/index.jsx";
 import Classcom from "./components/Classcom/index.jsx";
 import Refcom from "./components/Refcom/index.jsx";
 import Father from "./components/Fathercom/index.jsx";
+import ContextFatherToSon from './components/Context/index.jsx'
 function App() {
   return (
     <div id="box">
@@ -10,10 +11,7 @@ function App() {
       <Classcom></Classcom>
       <Refcom></Refcom>
       <Father></Father>
-      
-
-
-      
+      <ContextFatherToSon></ContextFatherToSon>
     </div>
   );
 }
---
+++ React/react-demo/src/components/Context/index.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+let {Provider, Consumer} = React.createContext();
+
+export default class ContextFatherToSon extends Component {
+
+
+  render() {
+    return (
+        <div style={{background: "pink", width: "500px", height: "500px"}}>
+          <Provider value="Provider上面的值">
+            <Consumer>
+              {
+                /* 渲染 Provider 上 value 的值 */
+                (data) => {
+                  return <span>{data}</span>
+                }
+              }
+            </Consumer>
+          </Provider>
+        </div>
+    )
+  }
+}
