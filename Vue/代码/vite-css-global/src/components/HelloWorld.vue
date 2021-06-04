7566a6982712d6fee86f1731b24e6db28754be3b
Silence-dream <1428482231@qq.com>
2021-06-04 15:20:19 +0800

    vite全局css

---
+++ Vue/代码/vite-css-global/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 class="hello">{{ msg }}</h1>
+
+ 
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+</script>
+
+<style lang="scss" scoped>
+.hello{
+  color: $color;
+}
+</style>
