a693157cee5ccc7425a4ea2fbf92f60f8e1ce0bf
Silence-dream <1428482231@qq.com>
2021-06-22 15:28:19 +0800

    react-hooks

--- React/react-hooks/src/components/UseEffect.jsx
+++ React/react-hooks/src/components/UseEffect.jsx
@@ -1,8 +1,28 @@
+import { useState, useEffect } from "react";
+
 const UseEffect = () => {
+  let [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // componentDidMont
+    console.log("数据渲染了");
+    // componentDidUpdate
+    console.log("数据更新了");
+    // componentWillUmount
+    return () => {
+      console.log("组件卸载了");
+    };
+  });
+  // 监听 count 更新
+  useEffect(() => {
+    console.log("count 更新了", count);
+  }, [count]);
+
   return (
-    <div>
-      <h1>useEffect</h1>
-      <div></div>
+    <div className="use-state" style={{ border: "1px solid pink" }}>
+      <h1>这里是 UseEffect 组件</h1>
+      <div>{count}</div>
+      <button onClick={() => setCount(count + 1)}>点击累加</button>
     </div>
   );
 };
