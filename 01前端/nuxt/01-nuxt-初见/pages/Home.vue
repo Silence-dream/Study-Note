0c14ed8278a2bd9cd09b7b22e5af6dd0c3c0de11
Silence-dream <1428482231@qq.com>
2021-09-26 21:01:00 +0800

    nuxt

---
+++ 01前端/nuxt/01-nuxt-初见/pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="box">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ 01前端/nuxt/01-nuxt-初见/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="box">
+    <h1>这里是index</h1>
+    <h1>这里是index</h1>
+    <h1>这里是index</h1>
+    <router-link to="/home">home</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
