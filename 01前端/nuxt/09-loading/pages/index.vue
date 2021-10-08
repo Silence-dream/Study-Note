42c6ac1d16e7e755ce665ecdbdadbf95a69f04d9
Silence-dream <1428482231@qq.com>
2021-10-08 20:27:33 +0800

    nuxt-loading

---
+++ 01前端/nuxt/09-loading/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="box">
+    <h1>index首页</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    // DOM 更新完毕之后调用 loading
+    this.$nextTick(() => {
+      // 调用 loading 中的方法,记得在 nux.config.js 里面配置
+      this.$nuxt.$loading.start();
+      setTimeout(() => this.$nuxt.$loading.finish(), 3000);
+    });
+  },
+};
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+}
+
+#box {
+  width: 800px;
+  height: 500px;
+  margin: 0 auto;
+  border: 1px solid;
+  text-align: center;
+}
+</style>
