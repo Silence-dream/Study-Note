11f7dda0581120776fcf9fefb99e4064556dd652
Silence-dream <1428482231@qq.com>
2021-10-11 21:27:16 +0800

    nuxtServerInit

---
+++ 01前端/nuxt/11/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class='box'>
+    <h1>{{ $store.state.msg }}</h1>
+    <button type='submit' @click='fn'>按钮</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    fn() {
+      // console.log(this.$store.state.msg)
+    }
+  }
+}
+</script>
---
+++ 01前端/nuxt/11/store/index.js
@@ -0,0 +1,10 @@
+export const state = function() {
+  return {
+    msg: '123213'
+  }
+}
+export const actions = {
+  nuxtServerInit(context,payload) {
+
+  }
+}
