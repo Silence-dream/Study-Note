036b76b1b28aea255a7157ed88a61b70647fbf25
silence_dream <1428482231@qq.com>
2022-05-12 21:28:49 +0800

    bind todo

---
+++ 07牛客/code/13-myBind.js
@@ -0,0 +1,19 @@
+Function.prototype.Mybind = function (fn) {
+  let _this = this
+  return function () {
+    return fn.apply(fn)
+  }
+}
+
+
+function test1() {
+  this.name = "test1"
+}
+
+function test2() {
+  console.log(this.name, [...arguments])
+}
+
+
+let result = test2.bind(test1, 1, 2, 3)
+result()
