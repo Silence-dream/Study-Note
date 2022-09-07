554fc7368df569ec9afd07934768e1fb0856a88f
Silence-dream <1428482231@qq.com>
2022-09-07 15:12:33 +0800

    vite Esbuild

--- Obsidian/我的笔记/编程/前端/构建工具/Vite/Esbuild/index.ts
+++
@@ -1,7 +0,0 @@
-let a =100;
-const b =200;
-const Hello=()=>{
-  console.log("Hello")
-}
-Hello();
-console.log(a,b)
---
+++ Obsidian/我的笔记/编程/前端/构建工具/Vite/Esbuild/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+let a = 100;
+const b = 200;
+let d = 2;
+(0.0).toPrecision(d);
+
+const Hello = () => {
+  console.log("Hello");
+  return (
+    <div className="123">
+      <h1>你好</h1>
+    </div>
+  );
+};
+
+Hello();
+console.log(a, b);
