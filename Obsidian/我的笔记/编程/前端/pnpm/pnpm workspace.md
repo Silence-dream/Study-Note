766738e221ffeb52872ba581ab865bfc29c3a34c
Silence-dream <lyqsl@foxmail.com>
2023-03-24 16:07:12 +0800

    update

---
+++ Obsidian/我的笔记/编程/前端/pnpm/pnpm workspace.md
@@ -0,0 +1,11 @@
+---
+tags : 
+- pnpm
+- workspace
+---
+
+如果我们想直接在直接包内部的命令
+```sh
+pnpm --filter 包名 script/下的,命令
+
+```
--- Obsidian/我的笔记/编程/区块链/code/07-Mapping.sol
+++ Obsidian/我的笔记/编程/区块链/code/07-Mapping.sol
@@ -1,2 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma
+pragma solidity ^0.8.4;
+
+contract Mapping {
+    mapping(uint256 => address) public idToAddress; // id映射到地址
+    mapping(address => address) public swapPair; // 币对的映射，地址到地址
+
+    // 我们定义一个结构体 Struct
+    struct Student {
+        uint256 id;
+        uint256 score;
+    }
+    mapping(Student => uint256) public testVar;
+
+    function writeMap(uint256 _Key, address _Value) public {
+        idToAddress[_Key] = _Value;
+    }
+
+}
