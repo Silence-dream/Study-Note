b8cb08560e57f67db9089f34bc0b3659aaaca834
Silence-dream <lyqsl@foxmail.com>
2023-10-13 17:58:04 +0800

    日志

--- Obsidian/我的笔记/编程/后端/NestJS/Code/project/src/main.ts
+++ Obsidian/我的笔记/编程/后端/NestJS/Code/project/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  // LoggerMiddleware,
+  logger,
+} from './middleware/logger/logger.middleware';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  // app.use(new LoggerMiddleware().use);
+  app.use(logger);
   // 全局路由前缀
   app.setGlobalPrefix('api');
   await app.listen(3000);
