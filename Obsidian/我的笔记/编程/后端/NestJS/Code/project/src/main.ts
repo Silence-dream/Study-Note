92ce025964dc4dfea562911806428ca2d3aab725
Silence-dream <lyqsl@foxmail.com>
2023-10-13 18:32:00 +0800

    拦截器

--- Obsidian/我的笔记/编程/后端/NestJS/Code/project/src/main.ts
+++ Obsidian/我的笔记/编程/后端/NestJS/Code/project/src/main.ts
@@ -5,6 +5,7 @@ import {
   logger,
 } from './middleware/logger/logger.middleware';
 import * as express from 'express';
+import { TransformInterceptor } from './interceptor/transform/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +14,11 @@ async function bootstrap() {
   app.use(express.urlencoded({ extended: true }));
 
   // app.use(new LoggerMiddleware().use);
+
+  // 日志
   app.use(logger);
+  // 全局拦截器
+  app.useGlobalInterceptors(new TransformInterceptor());
   // 全局路由前缀
   app.setGlobalPrefix('api');
   await app.listen(3000);
