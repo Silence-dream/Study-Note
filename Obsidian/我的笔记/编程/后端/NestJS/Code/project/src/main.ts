a2decf5615da92168ab1d3fc5ff863d515e1a8ab
Silence-dream <lyqsl@foxmail.com>
2023-10-16 14:45:39 +0800

    异常处理

--- Obsidian/我的笔记/编程/后端/NestJS/Code/project/src/main.ts
+++ Obsidian/我的笔记/编程/后端/NestJS/Code/project/src/main.ts
@@ -6,6 +6,8 @@ import {
 } from './middleware/logger/logger.middleware';
 import * as express from 'express';
 import { TransformInterceptor } from './interceptor/transform/transform.interceptor';
+import { HttpExceptionFilter } from './filter/http-exception/http-exception.filter';
+import { AllExceptionsFilter } from './filter/any-exception/any-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +23,10 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TransformInterceptor());
   // 全局路由前缀
   app.setGlobalPrefix('api');
+
+  // 过滤处理 HTTP 异常
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
